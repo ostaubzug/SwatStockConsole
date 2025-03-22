f6d8117e88c30f537dd4fab468df7a6de34154f7
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2025-03-22 15:26:20 +0100

    add console interface

--- StockConsole/Program.cs
+++ StockConsole/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace StockConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var service = new APIService();
+            Console.WriteLine("Which stock do you want to know the price of?");
+
+            string stockSymbol = Console.ReadLine();
+            var price = await service.GetLastPrice(stockSymbol);
+
+            Console.WriteLine($"The last price is {price}");
+        }
+    }
+}
